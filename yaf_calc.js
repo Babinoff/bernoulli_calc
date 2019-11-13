16686a45a584a2eb34b4613119b202176069e938
Alex.Babinoff <alex.babinoff.cheat@gmail.com>
2019-11-13 23:33:25 +0300

    class param set, for array(num) create

--- yaf_calc.js
+++ yaf_calc.js
@@ -1,11 +1,20 @@
 class dice_poll{
-	constructor(dh="",dw="",da="No",df="No",ac=3) {
-		this.dh = dh;
-		this.dw = dw;
-		this.da = da;
-		this.df = df;
-		this.ac = ac;
+	constructor(atk=3, hit="", wnd="", arm="No", fnp="No") {
+		this.atk = atk;
+		this.hit = hit;
+		this.wnd = wnd;
+		this.arm = arm;
+		this.fnp = fnp;
 	}
+}
+function fc(n){
+	if(n===0||n===1){
+		return 1;
+	}
+	return n*fc(n-1);
+}
+function brn(p,k,n){
+	return (fc(n)/(fc(k)*fc(n-k)))*(p**k*(1-p)**(n-k))
 }
 exports.input = function (data){
 	let sprt = "||"
@@ -23,20 +32,31 @@ exports.input = function (data){
 	let dice_fnp = {"No":0,"2+":1,"3+":2,"4+":3,"5+":4,"6+":5};
 
 	let test_poll = new dice_poll();
+	if (list_of_str != "") {
+		test_poll.atk = parseInt(list_of_str[0],10)
+		if (list_of_str.length > 1){test_poll.hit = list_of_str[1]}
+		if (list_of_str.length > 2){test_poll.wnd = list_of_str[2]}
+		if (list_of_str.length > 3){test_poll.arm = list_of_str[3]}
+		if (list_of_str.length > 4){test_poll.fnp = list_of_str[4]}
+	}
 	// let test_text = " chat_id:"+ch_id;
 	test_text = "input: \n";
-	test_text += list_of_str.join("\n")
 	if (list_of_str != "") {
+		test_text += list_of_str.join("\n")
 		test_text += "\n"
 		test_text += "output:"
 		// test_poll.dh = list_of_str[0];
 		// test_poll.dw = list_of_str[1];
 		test_text += "\n"
-		test_text += dice_to_hit[list_of_str[0]];
+		test_text += dice_to_hit[test_poll.hit];
 		test_text += "\n"
-		test_text += dice_to_wund[list_of_str[1]];
-	};
+		test_text += dice_to_wund[test_poll.wnd];
+	} else {test_text += b_text}
 	
+	for (const x of Array(test_poll.atk).keys()){
+		test_text += "\n"
+		test_text += x
+	}
 
 	let answer = {
 			"method":"sendMessage",
