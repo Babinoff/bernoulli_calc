786066422b3392cf0fc0da5db05859f1f15bf77f
Alex.Babinoff <alex.babinoff.cheat@gmail.com>
2019-11-17 23:49:10 +0300

    refactoring

--- yaf_calc.js
+++ yaf_calc.js
@@ -50,7 +50,7 @@ function rand_dice(max, count = "1"){
 exports.input = function (data) {
 	//#region params
 	var test = false;  // TEST TEXT
-	let sprt = "||"
+	let sprt = " "
 	let body = JSON.parse(data.body);
 	let b_text = body.message.text;
 	let chat_id = body.message.chat.id;
@@ -63,80 +63,19 @@ exports.input = function (data) {
 		let dice_to_wund = { "1+": 6, "2+": 5, "3+": 4, "4+": 3, "5+": 2, "6+": 1 };
 		let dice_armor_save = { "No": null, "2+": 1, "3+": 2, "4+": 3, "5+": 4, "6+": 5 };
 		let dice_fnp = { "No": null, "2+": 1, "3+": 2, "4+": 3, "5+": 4, "6+": 5 };
-		var i_atk = 3
-		var i_hit = "4+"
-		var i_wnd = "4+"
-		var i_arm = "No"
-		var i_fnp = "No"
+		let i_atk = 3
+		let i_hit = "4+"
+		let i_wnd = "4+"
+		let i_arm = "No"
+		let i_fnp = "No"
 
-		if (b_text.includes(sprt) & b_text.indexOf('/roll') === -1) {
-			list_of_str = b_text.split(sprt);
+		if (b_text.includes("/roll") & b_text.includes(sprt)) {
+			text_answer = roll_funct(b_text, list_of_str, text_answer, sprt)
+		}
+		if (b_text.includes("/poll") & b_text.includes(sprt)){
+			text_answer = poll_funct(b_text, sprt, list_of_str, i_atk, i_hit, i_wnd, i_arm, i_fnp, dice_to_hit, dice_to_wund, dice_armor_save, dice_fnp, text_answer)
 		}
 
-		if (b_text.includes("/roll") & b_text.includes(" ")) {
-			list_of_str = b_text.split(" ")
-			if (list_of_str.length == 2){
-				let string_list = rand_dice(b_text.split(" ")[1])
-				text_answer += string_list.join("\n")
-			} else if (list_of_str.length > 2) {
-				let string_list = rand_dice(b_text.split(" ")[1], b_text.split(" ")[2])
-				text_answer += string_list.join("\n")
-			}
-			test_text += "list_of_str.length: " + list_of_str.length + "\n"// TEST TEXT
-			
-		}
-		if (list_of_str != "" & b_text.indexOf('/roll') === -1) {
-			test_text += "input: \n"
-			test_text += "\n list_of_str content = " + list_of_str.join("\n") // TEST TEXT
-			i_atk = to_int(list_of_str[0])
-			test_text += "\n list_of_str.length = " + list_of_str.length  // TEST TEXT
-			if (list_of_str.length > 1) {
-				i_hit = list_of_str[1]
-			} else {}
-			if (list_of_str.length > 2) {
-				i_wnd = list_of_str[2]
-			}
-			if (list_of_str.length > 3) {
-				i_arm = list_of_str[3]
-			}
-			if (list_of_str.length > 4) {
-				i_fnp = list_of_str[4]
-			}
-			let atk = i_atk
-			let hit = dice_to_hit[i_hit]
-			let wnd = dice_to_wund[i_wnd]
-			let arm = dice_armor_save[i_arm]
-			let fnp = dice_fnp[i_fnp]
-
-			let brn_p = dice_drop(hit, wnd, arm, fnp)
-			test_text += "\n brn_p = " + brn_p  // TEST TEXT
-			let ok_nums_list = []
-			let brnpsum = []
-			let n = atk
-			const reducer = (accumulator, currentValue) => accumulator + currentValue;
-			for (const k of Array(atk + 1).keys()) { //+1 нужно из за начала списка с 0
-				let brn_f = 100 * brn(brn_p, k, n)
-				brnpsum.push(brn_f)
-				let itogo_int = to_int(Math.round(brnpsum.reduce(reducer)))
-				test_text += "\n itogo_int = " + itogo_int  // TEST TEXT
-				let itogo_float = brnpsum.reduce(reducer)
-				test_text += "\n itogo_float = " + itogo_float  // TEST TEXT 
-				ok_nums_list.push([itogo_int, itogo_float])
-			}
-			let string_list = []
-			for (const [i, num] of ok_nums_list.entries()) {
-				if (i == 0) {
-					string_list.push(i + " wound s 0" + " to " + ok_nums_list[0][0])
-				}
-				else if (ok_nums_list[i - 1][0] < 99) {
-					string_list.push(i + " wound s " + (ok_nums_list[i - 1][0] + 1) + " to " + num[0])
-				}
-				else {
-					string_list.push(i + " wound s " + ok_nums_list[i - 1][1] + " to " + num[1])
-				}
-			}
-			text_answer += string_list.join("\n")
-		} else { text_answer += "\n input: \n" + b_text }
 
 		if (test) {
 			text_answer += "\n" + test_text  // TEST TEXT
@@ -166,5 +105,80 @@ exports.input = function (data) {
 		"isBase64Encoded": false
 	}
 	//#endregion
+}
+
+function poll_funct(b_text, sprt, list_of_str, i_atk, i_hit, i_wnd, i_arm, i_fnp, dice_to_hit, dice_to_wund, dice_armor_save, dice_fnp, text_answer) {
+	list_of_str = b_text.split(sprt);
+	if (list_of_str != "" & b_text.indexOf('/roll') === -1) {
+		test_text += "input: \n";
+		test_text += "\n list_of_str content = " + list_of_str.join("\n"); // TEST TEXT
+		list_of_str.splice(0, 1) // Удаляем нудевой индекс с /roll
+		i_atk = to_int(list_of_str[0]);
+		test_text += "\n list_of_str.length = " + list_of_str.length; // TEST TEXT
+		if (list_of_str.length > 1) {
+			i_hit = list_of_str[1];
+		}
+		else { }
+		if (list_of_str.length > 2) {
+			i_wnd = list_of_str[2];
+		}
+		if (list_of_str.length > 3) {
+			i_arm = list_of_str[3];
+		}
+		if (list_of_str.length > 4) {
+			i_fnp = list_of_str[4];
+		}
+		let atk = i_atk;
+		let hit = dice_to_hit[i_hit];
+		let wnd = dice_to_wund[i_wnd];
+		let arm = dice_armor_save[i_arm];
+		let fnp = dice_fnp[i_fnp];
+		let brn_p = dice_drop(hit, wnd, arm, fnp);
+		test_text += "\n brn_p = " + brn_p; // TEST TEXT
+		let ok_nums_list = [];
+		let brnpsum = [];
+		let n = atk;
+		const reducer = (accumulator, currentValue) => accumulator + currentValue;
+		for (const k of Array(atk + 1).keys()) { //+1 нужно из за начала списка с 0
+			let brn_f = 100 * brn(brn_p, k, n);
+			brnpsum.push(brn_f);
+			let itogo_int = to_int(Math.round(brnpsum.reduce(reducer)));
+			test_text += "\n itogo_int = " + itogo_int; // TEST TEXT
+			let itogo_float = brnpsum.reduce(reducer);
+			test_text += "\n itogo_float = " + itogo_float; // TEST TEXT 
+			ok_nums_list.push([itogo_int, itogo_float]);
+		}
+		let string_list = [];
+		for (const [i, num] of ok_nums_list.entries()) {
+			if (i == 0) {
+				string_list.push(i + " wound s 0" + " to " + ok_nums_list[0][0]);
+			}
+			else if (ok_nums_list[i - 1][0] < 99) {
+				string_list.push(i + " wound s " + (ok_nums_list[i - 1][0] + 1) + " to " + num[0]);
+			}
+			else {
+				string_list.push(i + " wound s " + ok_nums_list[i - 1][1] + " to " + num[1]);
+			}
+		}
+		text_answer += string_list.join("\n");
+	}
+	else {
+		text_answer += "\n input: \n" + b_text;
+	}
+	return text_answer;
+}
+
+function roll_funct(b_text, list_of_str, text_answer, sprt) {
+	list_of_str = b_text.split(sprt);
+	if (list_of_str.length == 2) {
+		let string_list = rand_dice(b_text.split(sprt)[1]);
+		text_answer += string_list.join("\n");
+	}
+	else if (list_of_str.length > 2) {
+		let string_list = rand_dice(b_text.split(sprt)[1], b_text.split(sprt)[2]);
+		text_answer += string_list.join("\n");
+	}
+	test_text += "list_of_str.length: " + list_of_str.length + "\n"; // TEST TEXT
+	return text_answer;// return { list_of_str, text_answer };
 }
 //#endregion main
